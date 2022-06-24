a3689184e21c5a87a5e0ef17222d4bfdb00fc28b
Linda Rodriguez Salamanca <linda.rodriguez12@hotmail.com>
2022-06-24 08:20:52 -0500

    Agregando lista de clientes

--- src/components/CreateUser/UserList.js
+++ src/components/CreateUser/UserList.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { Component, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 import {
   TableContainer,
   Table,
@@ -10,98 +12,87 @@ import {
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
- 
   tablaMaterial: {
-    minWidth: 700,
-  
+    minWidth: 800,
   },
 }));
 
-function Prueba() {
-  const classes = useStyles();
-  /*const baseUrl = "http://localhost:4000/createUser";
-  const [data, setData] = useState([]);
+// function selectProject(project) {
+//   console.log(project.project_name);
+// }
 
-  const peticionesGet = async () => {
-    await axios
+export default class Prueba extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+    };
+  }
+
+  getAllUsers() {
+    let baseUrl = "http://localhost:4000/getUsers";
+    axios
       .get(baseUrl)
       .then((response) => {
-        setData(response.data);
+        this.setState({ users: response.data });
       })
       .catch((error) => {
         console.log(error);
       });
+    console.log("users " + this.state.users[0]);
+  }
+  componentDidMount = () => {
+    this.getAllUsers();
   };
-*/
 
-  var data = [
-    {
-      user_name: "Maria",
-      user_last_name: "Rodriguez",
-      identity_document_type: "C",
-      identity_document_word: "1049623415",
-      birth_date: "18/05/1995",
-      salary: "2000000",
-      weekly_hours: "48",
-      user_email: "Maria23@hotmail.com",
-      phone_number: 3217885634,
-    },
-    {
-      user_name: "Juan",
-      user_last_name: "Perez",
-      identity_document_type: "C",
-      identity_document_word: "1049645679",
-      birth_date: "17/04/1995",
-      salary: "2000000",
-      weekly_hours: "48",
-      user_email: "Jperez27@hotmail.com",
-      phone_number: 3216543236,
-    },
-  ];
-  return (
-    <div class="Table">
-      <h3 class="LabelTitleComponent">Lista de empleados </h3>
-      <TableContainer>
-        <Table className={classes.tablaMaterial}>
-          <TableHead>
-            <TableRow>
-              <TableCell align="center">NOMBRE</TableCell>
-              <TableCell align="center">APELLIDO</TableCell>
-              <TableCell align="center">TIPO DE DOCUMENTO</TableCell>
-              <TableCell align="center">NUMERO DE DOCUMENTO</TableCell>
-              <TableCell align="center">FECHA NACIMIENTO</TableCell>
-              <TableCell align="center">SALARIO</TableCell>
-              <TableCell align="center">HORAS TRABAJADAS</TableCell>
-              <TableCell align="center">CORREO</TableCell>
-              <TableCell align="center">TELEFONO</TableCell>
-              <TableCell align="center">ACCIONES</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((celda) => (
-              <TableRow>
-                <TableCell align="left">{celda.user_name}</TableCell>
-                <TableCell align="center">{celda.user_last_name}</TableCell>
-                <TableCell align="center">
-                  {celda.identity_document_type}
-                </TableCell>
-                <TableCell align="center">
-                  {celda.identity_document_word}
-                </TableCell>
-                <TableCell align="center">{celda.birth_date}</TableCell>
-                <TableCell align="center">{celda.salary}</TableCell>
-                <TableCell align="center">{celda.weekly_hours}</TableCell>
-                <TableCell align="center">{celda.user_email}</TableCell>
-                <TableCell align="center">{celda.phone_number}</TableCell>
-                <TableCell align="center">
-                  <button className="buttonDelete">Eliminar</button>
-                </TableCell>
+  render() {
+    console.log(this.state.users);
+    return (
+      <div class="Table">
+        <h3 class="LabelTitleComponent">Lista de Empleados </h3>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow hover>
+                <TableCell align="center">NOMBRE</TableCell>
+                <TableCell align="center">APELLIDO</TableCell>
+                <TableCell align="center">TIPO DE DOCUMENTO</TableCell>
+                <TableCell align="center">NUMERO DE DOCUMENTO</TableCell>
+                <TableCell align="center">FECHA NACIMIENTO</TableCell>
+                <TableCell align="center">SALARIO</TableCell>
+                <TableCell align="center">HORAS TRABAJADAS</TableCell>
+                <TableCell align="center">CORREO</TableCell>
+                <TableCell align="center">TELEFONO</TableCell>
+                <TableCell align="center">ACCIONES</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
-  );
+            </TableHead>
+            <TableBody>
+              {this.state.users.map((celda) => {
+                console.log(celda.User_Id);
+                console.log(this.state.users);
+                return (
+                  <TableRow key={celda.User_Id}>
+                    <TableCell align="left">{celda.User_Name}</TableCell>
+                    <TableCell align="center">{celda.User_Last_Name}</TableCell>
+                    <TableCell align="center">
+                      {celda.identity_document_type}{" "}
+                    </TableCell>
+                    <TableCell align="center">{celda.Document_Id}</TableCell>
+                    <TableCell align="center">{celda.Birth_Date}</TableCell>
+                    <TableCell align="center">{celda.Salary}</TableCell>
+                    <TableCell align="center">{celda.Weekly_Hours}</TableCell>
+                    <TableCell align="center">{celda.User_Email}</TableCell>
+                    <TableCell align="center">{celda.Phone_Number}</TableCell>
+                    <TableCell align="center">
+                      <button className="buttonDelete">Eliminar</button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    );
+  }
 }
-export default Prueba;
