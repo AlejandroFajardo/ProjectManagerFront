24866c45b6d6d62474253bdb45b86fc0381364d1
Didier Alejandro Fajardo Annear <didierfajardo28@hotmail.com>
2022-08-12 04:11:48 -0500

    Updates

---
+++ precache-manifest.db09f6839cb101816e27be4036b25c72.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "cabedf9d6aa7745948af",
+    "url": "/ProjectManagerFront/static/css/main.0ebf404f.chunk.css"
+  },
+  {
+    "revision": "cabedf9d6aa7745948af",
+    "url": "/ProjectManagerFront/static/js/main.cabedf9d.chunk.js"
+  },
+  {
+    "revision": "5888597e8b0a2ec029f5",
+    "url": "/ProjectManagerFront/static/js/1.5888597e.chunk.js"
+  },
+  {
+    "revision": "9e038fa9003bf2960d52",
+    "url": "/ProjectManagerFront/static/css/2.05803c21.chunk.css"
+  },
+  {
+    "revision": "9e038fa9003bf2960d52",
+    "url": "/ProjectManagerFront/static/js/2.9e038fa9.chunk.js"
+  },
+  {
+    "revision": "bdbf00c2f929750a251c",
+    "url": "/ProjectManagerFront/static/js/runtime~main.bdbf00c2.js"
+  },
+  {
+    "revision": "b9df058903ee6d739d254750cc6b9512",
+    "url": "/ProjectManagerFront/static/media/logo.b9df0589.png"
+  },
+  {
+    "revision": "c3d6d8177be1458c0c066c87e127bf1e",
+    "url": "/ProjectManagerFront/static/media/client.c3d6d817.jpg"
+  },
+  {
+    "revision": "43248112054b7351119e83cc80a48584",
+    "url": "/ProjectManagerFront/static/media/logo2.43248112.png"
+  },
+  {
+    "revision": "9883e489017e9142c66b65682be41b16",
+    "url": "/ProjectManagerFront/static/media/arquitectura.9883e489.jpg"
+  },
+  {
+    "revision": "dcef4752db1f997db7ab2d20fc3f5de1",
+    "url": "/ProjectManagerFront/index.html"
+  }
+];
